a199db604aad58d9e5b359e9f2ed44dcf4fc6e30
Rodrigo Azevedo <52163635+rodrigobivarazevedo@users.noreply.github.com>
2023-07-18 09:33:03 +0000

    .

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from fastapi.responses import HTMLResponse
 from datetime import datetime
 import os
 from pydantic import BaseModel
+from fhir.resources.imagingstudy import ImagingStudy
+from fhir.resources.reference import Reference
+from fhir.resources.meta import Meta
 
 app = FastAPI()
 app.mount("/images", StaticFiles(directory="images"), name="images")
@@ -62,7 +65,7 @@ def dicom_to_img(mypath):
 
 dicom_to_img("dicom/")
 
-# question 1- 
+
 
 def get_dcm_files(mypath):
     dcm_files = []
@@ -103,7 +106,6 @@ def patient_images_html(patient_id: str):
     return generate_html_grid(filtered_metadata)
 
 
-# question 2- 
 
 @app.get("/api", response_model=list[dict])
 def get_all_metadata():
@@ -112,7 +114,8 @@ def get_all_metadata():
     metadata = []
     for file in dcm_files:
         metadata.append(file.to_json())
-    
+    if not metadata:
+        raise HTTPException(status_code=404, detail="Patient not found")
     return JSONResponse(content=metadata)
 
 
@@ -133,17 +136,6 @@ def get_patient_metadata(patient_id: str):
     
     
 
-
- 
-
-# b) If you had to make it interoperable, how would you go about it? Which tools and which FHIR resources would you use? (10 points)
-# To make the /api route interoperable and comply with the FHIR standard, we can use FHIR resources python library and the appropriate FHIR server or FHIR client tools.
-
-from fhir.resources.imagingstudy import ImagingStudy
-from fhir.resources.reference import Reference
-from fhir.resources.meta import Meta
-
-
 @app.get("/apifhir", response_model=list[ImagingStudy])
 def get_all_metadata():
     dcm_files = get_dcm_files("dicom/")
@@ -162,15 +154,6 @@ def get_all_metadata():
     return metadata
 
 
-# question 3 - 
-
-# a) Which cues can you find in both image names and metadata to help solve this problem? (5 points)
-
-
-# Patient ID: The most crucial cue to match the image names with metadata is the Patient ID. Each DICOM image should have a Patient ID, which should also be a part of the image name.
-
-# Study Date: The Study Date in the DICOM metadata could be compared with the date mentioned in the image names to identify potential mismatches.
-
 
 def check_image_name_mismatches(dcm_files, image_folder):
     mismatches = []
@@ -182,12 +165,12 @@ def check_image_name_mismatches(dcm_files, image_folder):
         image_name = f"{patient_id}.png"
         image_path = join(image_folder, image_name)
         if not isfile(image_path):
-            mismatches.append((file.SOPInstanceUID, patient_id, image_name))
+            mismatches.append((patient_id, image_name))
 
     return mismatches
 
 def fix_image_names(mismatches, image_folder):
-    for uid, patient_id, image_name in mismatches:
+    for  patient_id, image_name in mismatches:
         old_path = join(image_folder, image_name)
         new_name = f"{patient_id}.png"
         new_path = join(image_folder, new_name)
